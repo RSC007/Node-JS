ad5b258df5bd9e61602a8e6d3352c035f24e841b
prathamesh patil <prathameshpatil2400@gmail.com>
2022-08-29 16:21:51 +0530

    web server configuration

---
+++ index.js
@@ -0,0 +1,14 @@
+// const logEvents = require('./logEvents')
+
+// const EventEmitter = require('events')
+
+// class Emitter extends EventEmitter {};
+
+// const myEventEmitter = new Emitter()
+
+
+// myEventEmitter.on('log', (msg)=> logEvents(msg))
+
+// setTimeout(()=>{
+//     myEventEmitter.emit('log', 'You log the event message')
+// }, 1000)
