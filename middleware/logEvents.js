24cda40b901e9af61bc9ba129a09082fe4577a87
prathamesh patil <prathameshpatil2400@gmail.com>
2022-08-30 12:46:21 +0530

    Added the Middleware with CORS

---
+++ middleware/logEvents.js
@@ -0,0 +1,26 @@
+const { format } = require('date-fns');
+const fs = require('fs')
+const fsPromise = require('fs').promises
+const path = require('path')
+
+const logEvents = async (message, logName) => {
+    const logDate = `${format(new Date, 'yyyy-MM-dd\tHH:MM:SS')}` 
+    const logItem = `${logDate}\t${message}\n`
+
+    try{
+        if(!fs.existsSync(path.join(__dirname, '..', 'logs'))){
+            await fsPromise.mkdir(path.join(__dirname, '..', 'logs'))
+        }
+        await fsPromise.appendFile(path.join(__dirname, '..', 'logs', logName), logItem)
+    }catch (err) {
+        console.log(err);
+    }
+}
+
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t${req.headers.origin} ${req.url}`, 'reqLog.txt')
+    console.log(`${req.method} ${req.path}`)
+    next()
+}
+
+module.exports = {logger, logEvents}
--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cors = require('cors')
+const { logger } = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
 
 const PORT = process.env.PORT || 2500
+
+// custom middleware
+app.use(logger)
+
+// Cros Origin Source Sharing ( Its Kind of Third Party Middleware )
+// whiteList can only the origin which can access
+const whiteList = ['https://myWebsiteName.com', 'https://www.google.co.in', 'http://127.0.0.1: 5500', 'http://localhost:2500']
+const crosOptions = {
+    origin: (origin, callback) => {
+        if(whiteList.indexOf(origin) !== -1 || !origin){
+            callback(null, true)
+        }else{
+            callback(new Error('Not Allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(crosOptions))
+
+// Buid-in middleware to handle urlencoded data
+// in other words, form data;
+// content-type: application/x-www-form-uelencoded
+app.use(express.urlencoded({ extended: false}))
+
+// build-in middleware for json
+app.use(express.json())
+
+// server static files
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('^/$|index(.html)?', (req, res) =>{
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
@@ -42,9 +74,21 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three])
 
-app.get('/*', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'views', '404.html'))
+// Any route that made this far, follwing response will get respectively
+app.get('*', (req, res)=>{
+    res.status = 404
+    if(req.accepted('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    }else if(req.accepted('json')){
+        res.json({ error: '404 Not Found'})
+    }else {
+        res.type('txt').send('404 Not Found')
+    }
+
 })
+
+// Middleware respond the error if crosOptions send failed message
+app.use(errorHandler)
 
 
 app.listen(PORT, () => console.log(`Server running at PORT : ${PORT}`))
